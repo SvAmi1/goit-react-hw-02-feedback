713b60106692dec97cf2c6faa3827f25653c9dbd
SvAmi1 <svmoshko877@gmail.com>
2023-08-12 22:53:12 +0300

    add comp-ts

---
+++ src/components/Layout.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Layout = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 0 15px;
+  max-width: 900px;
+  margin: 0 auto;
+  background: #81b4b8;
+`;
